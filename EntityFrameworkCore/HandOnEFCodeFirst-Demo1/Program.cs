1ac9bc1ef90ca5f39a2cb6cec24edf818e66f127
santuparsi <santuparsi@gmail.com>
2022-03-23 10:41:11 +0530

    new_commit

---
+++ EntityFrameworkCore/HandOnEFCodeFirst-Demo1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HandOnEFCodeFirst_Demo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
